c29426bd3ebeb61ce2c0a42fa63bcb54c71cd4a0
Saud <s.s.alghamdi@hotmail.com>
2021-09-28 18:19:39 +0300

    add def numToEng(n)

--- main.py
+++ main.py
@@ -784,6 +784,52 @@ def date_formating(date):
 print(date_formating('\r\r2020-02-10\r\n'))
 
 # ------------------------------------------------------------------
+# function for converting numbers to english words from 0 to 1000
+def numToEng(n):
+
+    dicti= {'0': 'zero', '1': 'One', '2': 'Two', '3': 'Three', '4': 'Four', '5': 'Five', '6': 'Six', '7': 'Seven',
+           '8': 'Eight', '9': 'Nine', '10': 'Ten', '11': 'Eleven', '12': 'Twelve', '13': 'Thirteen',
+           '14': 'Fourteen', '15': 'Fifteen', '16': 'Sixteen', '17': 'Seventeen', '18': 'Eighteen', '19': 'Nineteen',
+           '20': 'Twenty', '30': 'Thirty', '40': 'Forty', '50': 'Fifty', '60': 'Sixty', '70': 'Seventy', '80': 'Eighty',
+           '90': 'Ninety', '100': 'Hundred',
+           }
+    hun = ''
+    tens = ''
+    ones = ''
+    how_many_digits = len(str(n))
+    stri = str(n)
+    if how_many_digits <= 3:
+        for i in range(how_many_digits):
+            if how_many_digits == 3:
+                hun = dicti[stri[0]].lower() + ' hundred '
+                stri = stri[1:]
+                how_many_digits -= 1
+            else:
+                if how_many_digits == 2:
+                    if stri[0] == '0':
+                        tens = ''
+                        stri = stri[1:]
+                        how_many_digits -= 1
+                    elif stri[0] == '1':
+                        tens = dicti[stri].lower()
+                    else:
+                        if stri[1] == '0':
+                            tens = dicti[stri[0:]].lower()
+                        else:
+                            tens = dicti[stri[0] + '0'].lower() + '-'
+                            stri = stri[1:]
+                            how_many_digits -= 1
+                else:
+                    if how_many_digits == 1:
+                        if stri[0] == '0' and hun != '':
+                            ones = ''
+                        else:
+                            ones = dicti[stri].lower()
+    else:
+        print('Out of range')
+    return (hun + tens + ones).strip()
+
+# ------------------------------------------------------------------
 # Je Code
 
 info = {
